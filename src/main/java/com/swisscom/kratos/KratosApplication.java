f414a9141343cd914a991a7369af4361ea1da7fa
denis.o <denis.o@linux.com>
2021-06-21 19:51:40 +0300

    Imported basic project

---
+++ src/main/java/com/swisscom/kratos/KratosApplication.java
@@ -0,0 +1,13 @@
+package com.swisscom.kratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KratosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swisscom/kratos/KratosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swisscom.kratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
